fc215d6b4feab8579cb1e876ab9c5682267c0e3c
ZY <yuhang.zhu@studio.unibo.it>
2024-12-24 21:31:58 +0100

    Update sensor-controller.ts

--- Backend/src/controllers/sensor-controller.ts
+++ Backend/src/controllers/sensor-controller.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from "express";
-import { connectionAttempts, TOPIC_NAMES } from "../utils/broker";
 import connection from "../utils/db";
 
-
+// Ottenere una lista dei sensori da usare in una lista HTML per poter scegliere lo storico di un sensore
 export const getAllSensors = async (req: Request, res: Response): Promise<void> => {
     connection.execute(
         `SELECT *
@@ -15,6 +14,7 @@ export const getAllSensors = async (req: Request, res: Response): Promise<void> 
     )
 }
 
+// Si ottiene lo storico per il sensore scelto tramite HTML
 export const getSensor = async (req: Request, res: Response): Promise<void> => {
     const { id } = req.params;
 
@@ -29,3 +29,26 @@ export const getSensor = async (req: Request, res: Response): Promise<void> => {
     )
 }
 
+/* Codice da modificare in javascript puro da modificare in vue */
+// WebSocket per i dati correnti 
+// const socket = new WebSocket("ws://localhost:3000");
+
+// Inizializzazione WebSocket 
+// function setupWebSocket() {
+//     socket.onopen = () => console.log("Connessione ok.");
+//     socket.onmessage = (event) => {
+//         // Parsing dei dati ricevuti
+//         const data = JSON.parse(event.data);
+
+//         // Se il dato che arrivo ha temp_limit o hum_limit significa che c'è un problema con i sensori quindi crea un popup di avviso
+//         if (data.temp_limit || data.hum_limit) {
+//             alert(`Il sensore ${data.sensor} ha superato il limite di ${data.temp_limit ? "temperatura" : "umidità"} 
+//                 settata come limite a ${data.temp_limit ? data.temp_limit : data.hum_limit}, 
+//                 registrando una valore di ${data.temp_limit ? data.temp : data.hum}`);
+//         // Altrimenti semplicmente aggiungi il dato agli altri
+//         } else {
+//             aggiungiDatoCorrente(data);
+//         }
+//     };
+//     socket.onerror = (error) => console.error("Errore WebSocket:", error);
+// }
