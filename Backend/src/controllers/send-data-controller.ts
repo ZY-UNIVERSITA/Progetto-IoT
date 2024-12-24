3dfa7057b1fe10172e41281e38f1e425f6e29a05
ZY <yuhang.zhu@studio.unibo.it>
2024-12-24 16:56:41 +0100

    Add API test without frontend

--- Backend/src/controllers/send-data-controller.ts
+++ Backend/src/controllers/send-data-controller.ts
@@ -3,14 +3,20 @@ import { connectionAttempts, TOPIC_NAMES } from "../utils/broker";
 import connection from "../utils/db";
 
 export const modify_limit = (req: Request, res: Response) => {
-    const { limiteTemperatura, limiteUmidita} = req.body; // Recupera il valore 'limite' dal corpo della richiesta
-    console.log("Valore limite ricevuto:", limiteTemperatura, limiteUmidita); // Stampa il valore ricevuto per conferma
+    const { limits } = req.body; // Recupera l'array 'limits' dal corpo della richiesta
+    console.log("Valore limite ricevuto:", limits); // Stampa il valore ricevuto per conferma
     
-    connectionAttempts.getClient().publish(TOPIC_NAMES[1], JSON.stringify(
-        { 
-            "limiteTemperatura": limiteTemperatura, 
-            "limiteUmidita": limiteUmidita 
-        }
+    connectionAttempts.getClient().publish(TOPIC_NAMES.SERVER_TO_ESP32, JSON.stringify(limits));
+    res.status(200).send("ok");
+}
+
+export const modify_lim = (req: Request, res: Response) => {
+    connectionAttempts.getClient().publish(TOPIC_NAMES.SERVER_TO_ESP32, JSON.stringify(
+        [{ 
+            "sensor": 1,
+            "temp": 50, 
+            "hum": 10 
+        }]
     ));
     res.status(200).send("ok");
 }
--- Backend/src/routes/sendDataToSensor-router.ts
+++ Backend/src/routes/sendDataToSensor-router.ts
@@ -4,5 +4,6 @@ import * as sendDataController from "../controllers/send-data-controller";
 const router: Router = Router();
 
 router.post("/api/modifier/limit", sendDataController.modify_limit)
+router.get("/api/modifier/test", sendDataController.modify_lim)
 
 export default router;
