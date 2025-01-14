45988825efcbb5cb35567b9b2d0556daa020b95a
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-01-14 21:26:02 +0100

    Update config page style

--- Frontend/src/pages/Config.vue
+++ Frontend/src/pages/Config.vue
@@ -31,12 +31,35 @@ export default defineComponent({
 <template>
     <div class="configuration">
       <h2>Sensor Configuration</h2>
-      <label for="sensorId">Sensor ID:</label>
-      <input type="number" v-model="sensorId" />
-      <label for="tempLimit">Temperature Limit:</label>
-      <input type="number" v-model="tempLimit" />
-      <label for="humLimit">Humidity Limit:</label>
-      <input type="number" v-model="humLimit" />
-      <button @click="updateLimits">Update Limits</button>
+      <section class="configData">
+        <label for="sensorId">Sensor ID:</label>
+        <input type="number" v-model="sensorId" />
+        <label for="tempLimit">Temperature Limit:</label>
+        <input type="number" v-model="tempLimit" />
+        <label for="humLimit">Humidity Limit:</label>
+        <input type="number" v-model="humLimit" />
+        <button @click="updateLimits">Update Limits</button>
+      </section>
     </div>
 </template>
+
+<style lang="css" scoped>
+.configData {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  font-size: 20px;
+}
+
+input {
+  padding: 5px;
+  border-radius: 5%;
+}
+
+button {
+  background-color: blue;
+  color: white;
+}
+</style>
