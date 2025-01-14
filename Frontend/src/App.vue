2e4c62be775bf252575a7d2bdb2de909e0488cac
Nataliia Skybun <nataliia.skybun@studio.unibo.it>
2025-01-14 12:30:57 +0100

    Update navbar

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -4,10 +4,19 @@
       <h1>IoT Monitor</h1>
       <nav>
         <router-link to="/">Dashboard</router-link>
-        <router-link to="/history">History</router-link>
         <router-link to="/config">Configuration</router-link>
       </nav>
     </header>
     <router-view/>
   </div>
 </template>
+
+<style lang="css" scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+  font-size: 20px;
+}
+</style>
